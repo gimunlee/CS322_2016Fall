9e70735dfb9c2019a2cebc0657270881dcbced1e
gimun lee <gimunlee@kaist.ac.kr>
2016-10-03 18:13:51 +0900

    P02 completed

--- P02/P02.py
+++ P02/P02.py
@@ -5,12 +5,13 @@ DEBUG=False # flag for debug printing.
 import sys
 sys.path.append("../")
 
-dfaFilePath=""
+mealyFilePath=""
 inputFilePath=""
 outputFilePath=""
-# flag for path of dfa file
-if '--dfa' in sys.argv : dfaFilePath=sys.argv[sys.argv.index('--dfa')+1] 
-else :  print("The path of DFA description file not found"); quit(-1)
+
+# flag for path of mealy machine file
+if '--mealy' in sys.argv : mealyFilePath=sys.argv[sys.argv.index('--mealy')+1]
+else : print("The path of the mealy machine description file not found"); quit(-1) 
 
 # flags for paths of input and output files
 if '--input' in sys.argv : inputFilePath=sys.argv[sys.argv.index('--input')+1]
@@ -25,36 +26,35 @@ if '--debug' in sys.argv :
   else :
     DEBUG=False
 
-import dfa
-# read DFA from file
-dfa.read(dfaFilePath)
+import mealy
+# read Mealy machine from file
+mealy.read(mealyFilePath)
 
 outputFile=open(outputFilePath,'w')
 inputFile=open(inputFilePath,'r')
 for line in inputFile.readlines() :
+  if line=="end" :
+    break
+  outputLine = ""
   if DEBUG : print("================"); sys.stdout.write(line)
-  currentState=dfa.initState
+  currentState=mealy.initState
   for char in line :
     # end of line
     if char == '\n' : 
-      if DEBUG :
-        if dfa.isFinal(currentState) : print("네")
-        else : print("아니오")
-      if dfa.isFinal(currentState) : outputFile.write("네\n")
-      else : outputFile.write("아니오\n")
+      if DEBUG : print(outputLine)
+      outputFile.write(outputLine+"\n")
       break
     # character in line
-    if (currentState,char) in dfa.delta :
+    if (currentState,char) in mealy.delta :
       # transition exists
-      if DEBUG : print("%3s to %3s by %s"%(currentState,dfa.delta[currentState,char],char))
-      currentState=dfa.delta[currentState,char]
+      if DEBUG : print("%3s to %3s by %s"%(currentState,mealy.delta[currentState,char],char))
+      if (currentState,char) in mealy.lamb :
+        outputLine=outputLine+mealy.lamb[currentState,char]
+      currentState=mealy.delta[currentState,char]
     else :
-      if DEBUG : print("no transition")
-      if DEBUG :
-        if dfa.isFinal(currentState) : print("네")
-        else : print("아니오")
-      if dfa.isFinal(currentState) : outputFile.write("네\n")
-      else : outputFile.write("아니오\n")
+      # no transition
+      if DEBUG : print("No path exists!")
+      outputFile.write("No path exists!"+"\n")
       break
 outputFile.close()
 inputFile.close()
